f9b2f9876bacd7a79d5d95f62ab8f6c3e8454d32
Aleksandar Milic <alexmilic01@gmail.com>
2020-10-25 17:28:06 +0100

    Highscore implement

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 'use strict';
 
 let score = 20;
-
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
-document.querySelector('.number'). textContent = secretNumber;
+let highscore = 0;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 
 document.querySelector('.check').addEventListener('click', function() {
     const guess = Number(document.querySelector('.guess').value);
@@ -12,8 +11,15 @@ document.querySelector('.check').addEventListener('click', function() {
         document.querySelector('.message').textContent = '🚫 No number!';
     } else if(guess === secretNumber) {
         document.querySelector('.message').textContent = '🎉 Correct number!'
+        document.querySelector('.number'). textContent = secretNumber;
         document.querySelector('body').style.backgroundColor = '#60b347';
         document.querySelector('.number').style.width = '30rem';
+
+        if(score > highscore) {
+            highscore = score;
+            document.querySelector('.highscore').textContent = highscore;
+        }
+
     } else if(guess > secretNumber) {
         if(score > 1) {
             document.querySelector('.message').textContent = 'Too high!'
@@ -33,4 +39,17 @@ document.querySelector('.check').addEventListener('click', function() {
             document.querySelector('.score').textContent = 0;
         }
     }
+});
+
+// restart game
+document.querySelector('.again').addEventListener('click', function() {
+    score = 20;
+    secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+    document.querySelector('.message').textContent = 'Start guessing...';
+    document.querySelector('.score').textContent = score;
+    document.querySelector('.number').textContent = '?';
+    document.querySelector('.guess').value = '';
+    document.querySelector('body').style.backgroundColor = '#222';
+    document.querySelector('.number').style.width = '15rem';
 });
