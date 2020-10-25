3184052ef52a8ebd53524b6b501e3c0a239c6706
Aleksandar Milic <alexmilic01@gmail.com>
2020-10-25 16:45:29 +0100

    Implement game logic

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
 'use strict';
+
+let score = 20;
+
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number'). textContent = secretNumber;
+
+document.querySelector('.check').addEventListener('click', function() {
+    const guess = Number(document.querySelector('.guess').value);
+    
+    if(!guess) {
+        document.querySelector('.message').textContent = '🚫 No number!';
+    } else if(guess === secretNumber) {
+        document.querySelector('.message').textContent = '🎉 Correct number!'
+    } else if(guess > secretNumber) {
+        if(score > 1) {
+            document.querySelector('.message').textContent = 'Too high!'
+            score--;
+            document.querySelector('.score').textContent = score;
+        } else {
+            document.querySelector('.message').textContent = '💥 You lost the game!'
+            document.querySelector('.score').textContent = 0;
+        }
+    } else if(guess < secretNumber) {
+        if(score > 1) {
+            score--;
+            document.querySelector('.score').textContent = score;
+            document.querySelector('.message').textContent = 'Too low!'
+        } else {
+            document.querySelector('.message').textContent = '💥 You lost the game!'
+            document.querySelector('.score').textContent = 0;
+        }
+    }
+});
